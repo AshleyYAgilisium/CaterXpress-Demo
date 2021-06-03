27ca0ccd42a7bc95c796cb27e2243e4de647d7e8
AshleyYAgilisium <84870505+AshleyYAgilisium@users.noreply.github.com>
2021-06-02 18:51:38 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@
 // Copy the amplifyapp id and replace the invokeUrl below. 
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_o94MS7uKo', 
-        userPoolClientId: '1ecr6cuh47pk3l468hvb3j6arh', 
+        userPoolId: 'us-east-1_o3ltYsR6E', 
+        userPoolClientId: '2t9vuejmam4rupa4gkal4sdsld', 
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://master.d3bmggs9fwan3h.amplifyapp.com',
+        invokeUrl: 'https://master.d1twsvtm37y4qv.amplifyapp.com',
     }
 };
